c02ad4df5a8c934cd791d5fc106348c16599dd10
grace <grace@GraceDell>
2022-11-07 15:33:56 -0500

    Master

---
+++ hertzfgc_Assignment02/hertzfgc_Assignment02.py
@@ -0,0 +1,42 @@
+'''
+Name: Grace Hertzfeld
+email: hertzfgc@mail.uc.edu
+Assignment: Assignment 02
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This project demonstrates that I can implement p-code and utilize comments
+Citations:
+Anything else that's relevant:
+'''
+
+alpha = 100
+print(alpha)
+#print(type(alpha))
+#1. Alpha is an integer
+
+beta = "University of Cincinnati Bearcats"
+#print(type(beta))
+#2. Beta is a string
+
+print(beta + ' ' + str(alpha))
+
+delta = 1.5
+#print(type(delta))
+#3. Delta is a float
+
+gamma = alpha + delta
+print(gamma)
+#print(type(gamma))
+#4. Gamma is a float
+
+epsilon = beta[14]
+print(">" + epsilon + "<")
+
+
+alpha = "12345"
+print(alpha)
+#print(type(alpha))
+#5. Alpha is a string
+
+zeta = float(alpha) + delta
+print(zeta)
